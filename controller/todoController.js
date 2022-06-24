9d516b06a4c56ddc1c410657b0d129dc20e760b5
prxncxss03 <pocon.evangeline@gmail.com>
2022-06-24 22:23:26 +0800

    test again

--- controller/todoController.js
+++ controller/todoController.js
@@ -11,6 +11,16 @@ function isValidObjectId(id){
     return false;
 }
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://prxncxss03.github.io');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 const createMessage = async (req,res) => {
     const { text ,author,color} = req.body;
     const newTodo = new Todo({
