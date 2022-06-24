f6ed8c5d474ad6858196222307e7e367f549c748
prxncxss03 <pocon.evangeline@gmail.com>
2022-06-24 20:04:51 +0800

    update cors

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ dotenv.config({path : './.env'})
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoutes');
 const { default: mongoose } = require('mongoose');
-app.use(cors());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
