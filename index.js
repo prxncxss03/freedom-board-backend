7ea073937ee3ce5087a07ebde506d2dc4a00745d
prxncxss03 <pocon.evangeline@gmail.com>
2022-06-24 19:12:30 +0800

    ayaw pa rin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use('/api', todoRoutes);
 
-mongoose.connect(process.env.DATABASE_URL,
+mongoose.connect(process.env.MONGO_URI,
     {
         //must add in order to not get any error masseges:
         useNewUrlParser: true,
