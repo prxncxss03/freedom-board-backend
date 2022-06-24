2c05efddd6e79f36784ebb6bac78fe1d660b1a27
prxncxss03 <pocon.evangeline@gmail.com>
2022-06-24 22:45:18 +0800

    dfjdk

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 
 let corsOptions = {
-    origin: _*,
+    origin: '*',
     optionsSuccessStatus: 200, // For legacy browser support
     methods: "GET, PUT, POST, DELETE,OPTIONS",
     
