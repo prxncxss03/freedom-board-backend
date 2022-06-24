316716174b924468574a6c1b7c204b07e99f3e2a
prxncxss03 <pocon.evangeline@gmail.com>
2022-06-24 21:32:46 +0800

    add setheader

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ let corsOptions = {
 }
 
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'https://prxncxss03.github.io');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
 app.use('/api', todoRoutes);
 
 
