4bb848e04a798166c316a5649fc8cc5263a1cbfd
prxncxss03 <pocon.evangeline@gmail.com>
2022-06-24 21:40:23 +0800

    set credenteial;

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'https://prxncxss03.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', false);
 
     next();
 });
